787cb128dcad39264f63f213b1f8c937b7ea6345
Izabela Marcinkowska <izabela.marcinkowska@appliedtechnology.se>
2023-11-10 20:17:45 +0100

    feat: Create unsplashSearch function

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
 import "./style.css";
+
+const key = import.meta.env.VITE_ACCESS_KEY;
+
+const unsplashSearch = async (searchQuery: string) => {
+  const fetchResult = await fetch(
+    `https://api.unsplash.com/search/photos?query=${searchQuery}&client_id=${key}`
+  );
+  const data = await fetchResult.json();
+  return data;
+};
+
+unsplashSearch("bird").then((result) => {
+  console.log(result);
+});
