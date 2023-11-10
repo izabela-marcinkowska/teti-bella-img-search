f327f6770ac36f5e1a2891c95bb760901cc6feee
Izabela Marcinkowska <izabela.marcinkowska@appliedtechnology.se>
2023-11-10 21:40:31 +0100

    feat: Store search results and search

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import "./style.css";
 
 const key = import.meta.env.VITE_ACCESS_KEY;
 
+const searchForm = document.querySelector("#searchForm") as HTMLFormElement;
+console.log(searchForm);
+
 const unsplashSearch = async (searchQuery: string) => {
   const fetchResult = await fetch(
     `https://api.unsplash.com/search/photos?query=${searchQuery}&client_id=${key}`
@@ -10,6 +13,32 @@ const unsplashSearch = async (searchQuery: string) => {
   return data;
 };
 
-unsplashSearch("bird").then((result) => {
-  console.log(result);
+const storeSearchQuery = (searchQuery: string) => {
+  const storedSearchQuery = localStorage.getItem("prevSearches");
+  const parsedQueries = JSON.parse(storedSearchQuery as string) || [];
+  parsedQueries.push(searchQuery);
+  localStorage.setItem("prevSearches", JSON.stringify(parsedQueries));
+};
+
+const getPrevSearches = () => {
+  const storedSearchQuery = localStorage.getItem("prevSearches");
+  const parsedQueries = JSON.parse(storedSearchQuery as string) || [];
+  return parsedQueries;
+};
+
+const searches = getPrevSearches();
+console.log(searches);
+
+searchForm.addEventListener("submit", async function (e) {
+  e.preventDefault();
+  const formData = new FormData(e.target as HTMLFormElement);
+  const searchQuery = formData.get("searchQuery") as string;
+  const searchResult = await unsplashSearch(searchQuery);
+  storeSearchQuery(searchQuery);
+
+  console.log(searchResult);
 });
+
+// unsplashSearch("bird").then((result) => {
+//   console.log(result);
+// });
